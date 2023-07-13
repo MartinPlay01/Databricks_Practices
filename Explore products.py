6e22293426f34a7dc47dbf2ef92ec47e97b8a276
nitram2435@gmail.com <nitram2435@gmail.com>
2023-07-13 19:53:48 +0000

    Explore Azure Databricks
    Pyspark notebooks

---
+++ Explore products.py
@@ -0,0 +1,42 @@
+# Databricks notebook source
+df1 = spark.read.format("csv").option("header", "true").load("dbfs:/FileStore/shared_uploads/xlab-txz-998@xtremelabs.us/products_1_.csv")
+
+# COMMAND ----------
+
+display(df1)
+
+# COMMAND ----------
+
+df1.write.saveAsTable("products")
+
+# COMMAND ----------
+
+df1.count()
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT ProductName, ListPrice
+# MAGIC FROM products
+# MAGIC WHERE Category = 'Touring Bikes'
+
+# COMMAND ----------
+
+display(df1)
+
+# COMMAND ----------
+
+df1.printSchema()
+
+# COMMAND ----------
+
+df2 = df1.select("Category").distinct()
+display(df2)
+
+# COMMAND ----------
+
+
+from pyspark.sql.functions import col,sum
+df3 = df1.select("Category", col("ListPrice").cast("double").alias("ListPrice"))
+df4 = df3.groupBy("Category").agg(sum("ListPrice").alias("aggListPrice"))#.agg("ListPrice").count()
+display(df4)
